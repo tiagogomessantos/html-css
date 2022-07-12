a62d378129ffa918edad320de5d67178bc7d0b4c
tiagogomessantos <97561508+tiagogomessantos@users.noreply.github.com>
2022-07-11 22:10:38 -0300

    estudos

---
+++ exercicios/menuresponsivo/script.js
@@ -0,0 +1,8 @@
+function menu(){
+    let menumobile = document.querySelector('.mobile-menu')
+    if(menumobile.classList.contains('open')){
+        menumobile.classList.remove('open')
+        document.querySelector('.icon').src = "imagem/imagem.jgp;"
+    }else
+
+}
